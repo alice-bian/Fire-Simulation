175faf2bd95ad681bf9dbba2eb5dfb3a3f643f14
Alice Bian <alicebian@alumni.upenn.edu>
2024-12-19 02:53:04 -0800

    Built in timer and steps for capturing fire simulation

---
+++ src/open_gl_headers.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+// WINDOWS:
+#include <windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include "../../glut/glut.h"
+
+/*
+// MAC:
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+*/
+
